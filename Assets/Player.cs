5c9b2729b0e12ef2104fff8cccbd174f46c33131
diegobatiz <diegow.batiz@gmail.com>
2023-12-08 12:16:50 -0800

    game over screen

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -99,6 +99,7 @@ public class Player : MonoBehaviour
         if (_health <= 0)
         {
             Debug.Log("Game Over");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
         }
     }
 
