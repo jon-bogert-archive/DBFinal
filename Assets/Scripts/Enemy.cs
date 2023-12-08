1f05680754c264cb5bbe310180afe8f9bba0189b
diegobatiz <diegow.batiz@gmail.com>
2023-12-08 11:43:31 -0800

    prefabs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Enemy : MonoBehaviour
 {
 	[SerializeField] private float _speed;
 	[SerializeField] private float _damage;
 	[SerializeField] private float _health;
+	[SerializeField] private Rigidbody2D _rb2D;
 	
 	private Player _player;
 	
@@ -16,6 +18,7 @@ public class Enemy : MonoBehaviour
 	private void Update()
 	{
 		transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, _speed * Time.deltaTime);
+		_rb2D.velocity = new Vector2(0f, 0f);
 	}
 
 	public void AddHurt(int damage)
